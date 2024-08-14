7b324782a33c8031fd8449c59bba3f64abde0fa7
srv1n <sarav@gmx.com>
2024-08-14 13:12:57 +0530

    a

--- src/lib.rs
+++ src/lib.rs
@@ -3161,30 +3161,9 @@ pub fn output_doc(doc: &Document) -> Result<Vec<ContentOutput>, OutputError> {
 }
 
 pub fn parse_pdf(file: &str) -> Result<Vec<ContentOutput>, OutputError> {
-    let output_kind = "txt";
-    //let output_kind = "svg";
-
-    // let output_kind = env::args().nth(2).unwrap_or_else(|| "txt".to_owned());
-    // println!("{}", file);
     let path = path::Path::new(&file);
-    let filename = path.file_name().expect("expected a filename");
-    let mut output_file = PathBuf::new();
-    output_file.push(filename);
-    output_file.set_extension(&output_kind);
-    let mut output_file =
-        BufWriter::new(File::create(output_file).expect("could not create output"));
+
     let doc = Document::load(path).unwrap();
-
-    print_metadata(&doc);
-
-    let mut output: Box<dyn OutputDev> = match output_kind.as_ref() {
-        "txt" => Box::new(PlainTextOutput::new(
-            &mut output_file as &mut dyn std::io::Write,
-        )),
-        "html" => Box::new(HTMLOutput::new(&mut output_file)),
-        "svg" => Box::new(SVGOutput::new(&mut output_file)),
-        _ => panic!(),
-    };
 
     let document_structure = output_doc(&doc);
     document_structure
